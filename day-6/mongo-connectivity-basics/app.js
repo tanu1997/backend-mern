1e946d89d5fd56f19d277263f5c50ec6d24d9473
tanu1997 <tanupriya.srivastava@nbgspl.com>
2023-02-13 17:42:58 +0530

    nodejs: mongoose connectivity

--- day-6/mongo-connectivity-basics/app.js
+++ day-6/mongo-connectivity-basics/app.js
@@ -1,12 +1,38 @@
+// import the connector
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test');
+ /*try to get connected with the mongo (community) server running on the 
+-port : 27017
+-host : localhost
+-db   : test */
+ 
 
-const Cat = mongoose.model('Cat',{name:String}); //Model name Cat 
+mongoose.connect('mongodb://localhost:27017/test'); //create single connection only
+mongoose.set('debug', true);
 
-const kitty = new Cat({ name: 'Tanupriya' });  //Create object Kitty
+'Mongoose is ODM'
+'JS object is mapped with object data model'
 
+const Cat = mongoose.model('Cat',{name:String , isBig:Boolean }); //Model name Cat 
+
+// actual data
+const kitty = new Cat({ name: 'Tanupriya' , isBig: false});  //Create object Kitty
+
+
+/* mongoose will convert save call to the db insert query*/
 kitty.save()
     .then( () => console.log('Cat Saved') )
+    
     .catch( err => console.log('There is error') )
+   // .finally(() => mongoose.disconnect()) //need to disconnect only once
+
+//   Cat.find((err,cats)=>
+//   {
+//     if(err) return
+//     console.log(cats)
+//   })
+
+Cat.find({'isBig':false},{isBig : 0 },(err, cats)=>console.log(cats)).limit(2)
+   
 
