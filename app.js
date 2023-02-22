22f278f44b421876b0a60942373e4494937dcebd
tanu1997 <tanupriyasrivastava7@gmail.com>
2023-02-22 17:24:58 +0530

    testing

--- app.js
+++ app.js
@@ -9,12 +9,44 @@ mongoose.connect('mongodb://127.0.0.1/test').then(() => {
 }).catch(err => {
     console.log('There is a problem in connnection')
 }) 
-const Car = mongoose.model('Car',{
-    model : String,
-    country : Number,
-    isFwd : Boolean,
-    make : String
+
+const CarSchema = new mongoose.Schema(
+    {
+        model : {type : String ,unique : true},
+        country : {type: Number},
+        isFwd : { type : Boolean},
+        make : { type :String}
+    }
+)
+
+CarSchema.pre('save' ,next =>{
+    console.log('Pre : Middleware Executed')
+    next()
 })
+
+CarSchema.post('save' ,doc =>{
+    console.log('Post : Middleware Executed')
+    console.log('ID : ' +doc._id)
+   
+})
+
+CarSchema.post('save' ,(error,doc,next) =>{
+    console.log('Post : Middleware Executed')
+    console.log('ID : ' +doc._id)
+    if(error.name === 'MongoServerError' && error.code === 11000)
+    {
+        next(new Error('Model Should be Unique'))
+    }else
+    {
+        next()
+    }
+   
+})
+
+
+const Car = mongoose.model('Car',CarSchema)
+
+
 
 const express = require('express')
 const app = express()
@@ -26,27 +58,35 @@ app.post('/', express.json(),(req,res) =>
     const reqCar = req.body
     console.log(reqCar)
 
-    const dbObj = new Car({
-        model : reqCar.model,
-        make : reqCar.make,
-        isFwd:reqCar.isFwd,
-        country : reqCar.country
-    })
+    
+
+    const dbObj =new Car({...reqCar})
+    
+    
 
     dbObj.save()
     .then(
         sts => res.status(201).json({sts : 'success'})
     )
     .catch(
-        err =>res.status(400).json({sts : 'err' })
+        err =>
+        {
+           
+            res.status(400).json({sts : 'err' ,msg: err.message })
+        }
     )
 
-    // res.json({ sts : 'success'})
+   
 })
 
 app.get('/',(req,res) =>
 {
-    
+    const cars =Car.find({},{__v :0},(err,crs) =>
+    {
+        if(err) res.status(500).json()
+        res.json(crs)
+    },)
+   
 })
 
 app.put('/',(req,res) =>
