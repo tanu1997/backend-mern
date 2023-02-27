a08ccf67255fe6f8aeb257414796a984042cccc9
tanu1997 <tanupriyasrivastava7@gmail.com>
2023-02-27 15:37:22 +0530

    jwt-webapp: express integration with db

--- day-11/jwt-webapp/app.js
+++ day-11/jwt-webapp/app.js
@@ -1,16 +1,48 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const AppUserSchema = new mongoose.Schema({
+    userName: {type: String, unique: true},
+    password: {type: String},
+})
+
+const AppUser =mongoose.model('AppUser',AppUserSchema)
+
+const op = new AppUser({  userName : 'abc123', password: 'abc123'})
+op.save()
+
+
 // 1. Setup Express App
 
-const express = require('express')
+const express = require('express');
+const { response } = require('express');
 const app = express()
 
 
 
 app.post('/login',express.json(), (req, res)=>
 {
-    const body = req.body
-    console.log(body)
-    res.json({ sts : 'success', msg : 'Logged In Successfully'})
-    
+    const {userName,password} = req.body
+
+    AppUser.findOne({userName, password },(err, appUser)=>
+{
+    if(err)
+    {
+        console.log('✖ Db Error')
+        response.json({sts : 'Fail', msg : 'Db Error'})
+       
+    }
+    if(appUser == null)
+    {
+        console.log('✖ User not Found')
+        response.json({sts : 'Fail', msg : 'User not Found'})
+    }
+    console.log(appUser)
+    res.json({sts : 'success',token:'Not Implemented Yet', msg : '✔ User Logged In Successfully'})
+})
+
+
+   
 })
 
 app.listen(6000,() =>
@@ -19,4 +51,8 @@ app.listen(6000,() =>
 })
 
 // 2. Integrate with mongo
+
+
+
+
 // 3. Add JWT Security
